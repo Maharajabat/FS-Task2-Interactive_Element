5a6c6d2d0ae22954fe7f25eefbc31809d4b0cf4e
Maharajabat <123624747+Maharajabat@users.noreply.github.com>
2023-08-01 21:12:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function changeColor(){
+    alert("Press OK to change color!!")
+    document.getElementById("color").style.backgroundImage="url(Batmanbw.jpg)";
+    alert("Refresh to get Original Color!!");
+}
